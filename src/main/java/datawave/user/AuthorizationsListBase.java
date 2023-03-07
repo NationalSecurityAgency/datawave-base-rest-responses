93e13632a72313060e3a582e9fefa050634857ef
Ivan Bella <ivan@bella.name>
2023-03-07 12:41:59 -0500

    Feature/federate user operations (#1825)
        * re #1821: Added a remote user operations implementation
          * Updated the authorization mechanisms to validate against the overall
             user (which may include the remote authorizations) and then the reduce
             by the query authorizations.  This allows the auths returned by the list
             effective authorizations which include remote auths to be used for local
             query logics as well.
          * Added optional flag to the listEffectiveAuthorizations avoid federation
          * Updated Query to now know about a remote user operations
          * logics will use the remote user operation if set for query auths
          * Composite Query Logic will recalculate query auths for each logic
          * Added test cases for new classes and integration tests
          * Updated test cases to handle new query logic getUserOperations call

--- src/main/java/datawave/user/AuthorizationsListBase.java
+++ src/main/java/datawave/user/AuthorizationsListBase.java
@@ -42,6 +42,7 @@ public abstract class AuthorizationsListBase<T> implements Message<T>, HtmlProvi
     
     protected String userDn;
     protected String issuerDn;
+    protected List<String> messages = new ArrayList<>();
     
     protected LinkedHashMap<SubjectIssuerDNPair,Set<String>> auths = new LinkedHashMap<SubjectIssuerDNPair,Set<String>>();
     
@@ -64,14 +65,55 @@ public abstract class AuthorizationsListBase<T> implements Message<T>, HtmlProvi
         auths.put(new SubjectIssuerDNPair(dn, issuerDN), new TreeSet<String>(dnAuths));
     }
     
+    public void setMessages(List<String> messages) {
+        this.messages.clear();
+        if (messages != null) {
+            this.messages.addAll(messages);
+        }
+    }
+    
+    public void addMessage(String message) {
+        this.messages.add(message);
+    }
+    
     public void setUserAuths(String userDn, String issuerDn, Collection<String> userAuths) {
         this.userDn = userDn;
         this.issuerDn = issuerDn;
         this.userAuths.addAll(userAuths);
     }
     
+    public TreeSet<String> getUserAuths() {
+        return new TreeSet<String>(userAuths);
+    }
+    
     public TreeSet<String> getAllAuths() {
         return new TreeSet<String>(userAuths);
+    }
+    
+    public String getUserDn() {
+        // if the userDn is empty but we have auths, then use the first auth entry as the user
+        // this handles the case where the userDn and the subjectDn are not being serialized separately.
+        if (userDn == null && !auths.isEmpty()) {
+            return auths.keySet().iterator().next().subjectDN;
+        }
+        return userDn;
+    }
+    
+    public String getIssuerDn() {
+        // if the issuerDn is empty but we have auths, then use the first auth entry as the user
+        // this handles the case where the userDn and subjectDn are not being serialized separately.
+        if (issuerDn == null && !auths.isEmpty()) {
+            return auths.keySet().iterator().next().issuerDN;
+        }
+        return issuerDn;
+    }
+    
+    public List<String> getMessages() {
+        return messages;
+    }
+    
+    public Map<String,Collection<String>> getAuthMapping() {
+        return authMapping;
     }
     
     public void setAuthMapping(Map<String,Collection<String>> authMapping) {
@@ -211,6 +253,17 @@ public abstract class AuthorizationsListBase<T> implements Message<T>, HtmlProvi
                     }
                 }
             }
+            if (message.userDn != null) {
+                output.writeString(3, message.userDn, false);
+            }
+            if (message.issuerDn != null) {
+                output.writeString(4, message.issuerDn, false);
+            }
+            if (!message.messages.isEmpty()) {
+                for (String msg : message.messages) {
+                    output.writeString(5, msg, true);
+                }
+            }
         }
         
         @Override
@@ -236,6 +289,15 @@ public abstract class AuthorizationsListBase<T> implements Message<T>, HtmlProvi
                             message.authMapping.put(auths[0], new HashSet<String>());
                         message.authMapping.get(auths[0]).add(auths[1]);
                         break;
+                    case 3:
+                        message.userDn = input.readString();
+                        break;
+                    case 4:
+                        message.issuerDn = input.readString();
+                        break;
+                    case 5:
+                        message.messages.add(input.readString());
+                        break;
                     default:
                         input.handleUnknownField(number, this);
                         break;
@@ -250,6 +312,12 @@ public abstract class AuthorizationsListBase<T> implements Message<T>, HtmlProvi
                     return "auths";
                 case 2:
                     return "authMapping";
+                case 3:
+                    return "userDn";
+                case 4:
+                    return "issuerDn";
+                case 5:
+                    return "messages";
                 default:
                     return null;
             }
@@ -265,6 +333,9 @@ public abstract class AuthorizationsListBase<T> implements Message<T>, HtmlProvi
         {
             fieldMap.put("auths", 1);
             fieldMap.put("authMapping", 2);
+            fieldMap.put("userDn", 3);
+            fieldMap.put("issuerDn", 4);
+            fieldMap.put("messages", 5);
         }
     }
 }
